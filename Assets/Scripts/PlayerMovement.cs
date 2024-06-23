28fc649ea1429e7c15e12df6944e03a3ae37af82
slimesarecool <arinmanchannel@gmail.com>
2024-06-22 21:10:54 -0700

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,8 +8,7 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
-    public float acceleration = 0f;
-    public float accelerationLimit = 1f;
+    public float accelerationLimit = 0.5f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -17,6 +16,7 @@ public class PlayerMovement : MonoBehaviour {
 
     Vector3 velocity;
     bool isGrounded;
+    float acceleration = 0f;
 
     void Update() {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -26,10 +26,29 @@ public class PlayerMovement : MonoBehaviour {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        if (x + z > 0) {
+            if (!isGrounded) {
+                acceleration += 0.1f * Time.deltaTime;
+            } else {
+                acceleration += 0.05f * Time.deltaTime;
+            }
+
+            acceleration = Mathf.Clamp(acceleration, 0, accelerationLimit);
+        }
+
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
 
-        if (Input.GetButtonDown("Jump") && isGrounded) velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            acceleration = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            controller.height = 1.24f;
+        } else {
+            controller.height = 3.8f;
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
